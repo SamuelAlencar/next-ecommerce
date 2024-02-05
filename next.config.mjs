4153704d30ce7979fcb6ef0cfc8ddb4e849b56ac
samuel_alencar <samuel_alencar@carrefour.com>
2024-02-05 05:38:45 -0300

    product fetch on home

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['samuel-alencar-portifolio.vercel.app', 'xesque.rocketseat.dev'],
+    domains: [
+      'samuel-alencar-portifolio.vercel.app',
+      'xesque.rocketseat.dev',
+      'http://localhost:3000/',
+    ],
   },
 }
 
